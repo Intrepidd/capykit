d2b34a37f3f26be279b6348fe815e996bc530b11
Adrien Siami <adrien.siami@dimelo.com>
2015-02-12 17:04:49 +0100

    Initial commit

---
+++ spec/webkit_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Meh', :js => true, :type => :feature do
+
+  before do
+    visit '/'
+    puts page.body.size
+  end
+
+  it 'fails' do
+  end
+
+
+  it 'fails' do
+  end
+
+
+  it 'fails' do
+  end
+
+
+  it 'fails' do
+  end
+
+end
